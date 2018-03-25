2b5be06424f53158c48028e0652b1cfff8e42101
杨勇 <yongyang2@iflytek.com>
2018-03-25 20:08:37 +0800

    1111

---
+++ testApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testApplication
+//
+//  Created by 杨勇 on 2018/3/25.
+//  Copyright © 2018年 qqqq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testApplication
+//
+//  Created by 杨勇 on 2018/3/25.
+//  Copyright © 2018年 qqqq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
